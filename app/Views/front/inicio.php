3dd9da0e596049df4fb2d4739cb5841c54daa4f9
Toia <victoriacool124@gmail.com>
2025-04-23 00:56:22 -0300

    Nuevo carrusel inicio

--- app/Views/front/inicio.php
+++ app/Views/front/inicio.php
@@ -1,29 +1,48 @@
-<section class="container">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active" style="background-image: url('assets/img/20000Leagues.jpg'); background-size: cover; background-position: center; height: 500px;">
-                </div>
-                    <div class="carousel-item" style="background-image: url('assets/img/nosferatu.jpg'); background-size: cover; background-position: center; height: 500px;">
-                </div>
-                    <div class="carousel-item" style="background-image: url('assets/img/theGreatDictator.jpg'); background-size: cover; background-position: center; height: 500px;">
-                </div>
-            </div>
-
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-</section>
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets\img\nosferatu_carrusel.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="card" style="width: 18rem;">
+            <img src="assets\img\nosferatu.jpg" class="card-img-top">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets\img\theGhoul_carrusel.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="card" style="width: 18rem;">
+          <img src="assets\img\theGhoul.jpg" class="card-img-top">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets\img\duckSoup_carrusel.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="card" style="width: 18rem;">
+          <img src="assets\img\duckSoup.jpg" class="card-img-top">
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 <section> <!--cards-->
     <div>
     <p class="fs-3">Recomendados</p>

438bc5a18a5c4ab1761a3b92278fe98b7c8179cb
Toia <victoriacool124@gmail.com>
2025-04-25 16:53:55 -0300

    mejora de catalogo

--- app/Views/front/catalogo.php
+++ app/Views/front/catalogo.php
@@ -1,87 +1,85 @@
-<div>
-<p class="fs-3" id="terror">Terror</p>
-</div>
-<div class="row row-cols-1 row-cols-md-4 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/houseOnHauntedHill.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">House on the Haunted Hill</h5>
-        <p class="card-text">Un millonario ofrece diez mil dólares a cinco personas que accedan a permanecer encerradas en una casa grande y espeluznante durante una noche con él y su esposa.</p>
+<div class="container my-4">
+  <p class="fs-3" id="terror">Terror</p>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/houseOnHauntedHill.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">House on the Haunted Hill</h5>
+          <p class="card-text">Un millonario ofrece diez mil dólares a cinco personas que accedan a permanecer encerradas en una casa grande y espeluznante durante una noche con él y su esposa.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/nosferatu.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Nosferatu</h5>
+          <p class="card-text">El vampiro conde Orlok manifiesta su interés por una nueva residencia y por la esposa del agente inmobiliario Hutter.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/theGhoul.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">The Ghoul</h5>
+          <p class="card-text">Un egiptólogo regresa de entre los muertos para vengarse de quienes violaron su tumba.</p>
+        </div>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/nosferatu.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">Nosferatu</h5>
-        <p class="card-text">El vampiro conde Orlok manifiesta su interés por una nueva residencia y por la esposa del agente inmobiliario Hutter.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/theGhoul.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">The Ghoul</h5>
-        <p class="card-text">Un egiptólogo regresa de entre los muertos para vengarse de quienes violaron su tumba.</p>
-      </div>
-    </div>
-  </div>
-</div>
 
-<div>
-<p class="fs-3" id="ciencia-ficcion">Ciencia Ficcion</p>
-</div>
-<div class="row row-cols-1 row-cols-md-4 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/battleOfTheWorlds.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">La Batalla de los Mundos</h5>
-        <p class="card-text">Un pequeño pueblo de California es atacado por marcianos, lo que da comienzo a una invasión mundial.</p>
+  <p class="fs-3 mt-5" id="ciencia-ficcion">Ciencia Ficción</p>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/battleOfTheWorlds.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">La Batalla de los Mundos</h5>
+          <p class="card-text">Un pequeño pueblo de California es atacado por marcianos, lo que da comienzo a una invasión mundial.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/20000Leagues.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">20.000 Leguas de Viaje Submarino</h5>
+          <p class="card-text">Un barco enviado a investigar varios hundimientos misteriosos se encuentra un submarino, Nautilus, comandado por el capitán Nemo.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/thingsToCome.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Things To Come</h5>
+          <p class="card-text">La Segunda Guerra Mundial deja la peste y la anarquía, pero luego un estado racional reconstruye la civilización e intenta incursionar en los viajes espaciales.</p>
+        </div>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/20000Leagues.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">20.000 Leguas de Viaje Submarino</h5>
-        <p class="card-text">Un barco enviado a investigar varios hundimientos misteriosos se encuentra un submarino, Nautilus, comandado por el capitán Nemo.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/thingsToCome.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">Things To Come</h5>
-        <p class="card-text">La Segunda Guerra Mundial deja la peste y la anarquía, pero luego un estado racional reconstruye la civilización e intenta incursionar en los viajes espaciales.</p>
-      </div>
-    </div>
-  </div>
-</div>
 
-<div>
-<p class="fs-3" id="comedia">Comedia</p>
+  <p class="fs-3 mt-5" id="comedia">Comedia</p>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/myManGodfrey.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">My Man Godfrey</h5>
+          <p class="card-text">La rica heredera Irene contrata a Godfrey como mayordomo familiar sin saber de su misterioso pasado.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/theGreatDictator.jpg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">El Gran Dictador</h5>
+          <p class="card-text">El dictador Adenoid Hynkel trata de expandir su imperio mientras un humilde barbero judío intenta escapar de la persecución del régimen de Hynkel.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="row row-cols-1 row-cols-md-4 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/myManGodfrey.jpg" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">My Man Godfrey</h5>
-        <p class="card-text">La rica heredera Irene contrata a Godfrey como mayordomo familiar sin saber de su misterioso pasado.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="assets/img/theGreatDictator.jpg" class="card-img-top">
-        <h5 class="card-title">El Gran Dictador</h5>
-        <p class="card-text">El dictador Adenoid Hynkel trata de expandir su imperio mientras un humilde barbero judío intenta escapar de la persecución del régimen de Hynkel.</p>
-      </div>
-    </div>
-  </div>

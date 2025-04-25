8aa23c6bba9f69ede6e49095d7d7c881fee0524f
Lautaro Lezcano <lautarofranciscolezcano@gmail.com>
2025-04-25 16:06:06 -0300

    arreglo del carrusel

--- app/Views/front/inicio.php
+++ app/Views/front/inicio.php
@@ -4,36 +4,32 @@
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="assets/img/nosferatu_gif.gif" class="d-block w-100" alt="nosferatu_carrusel">
-      <div class="carousel-caption d-none d-md-block">
-        <div class="card" style="width: 18rem;">
-            <img src="assets\img\nosferatu.jpg" class="card-img-top">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/img/nosferatu_gif.gif" class="d-block w-100" alt="nosferatu_carrusel">
+            <div class="carousel-caption d-none d-md-block">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets\img\nosferatu.jpg" class="card-img-top">
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item">
-      <img src="assets/img/theGhoul_gif.gif" class="d-block w-100" alt="TheGhoul_carrusel">
-      <div class="carousel-caption d-none d-md-block">
-        <div class="card" style="width: 18rem;">
-          <img src="assets\img\theGhoul.jpg" class="card-img-top">
+        <div class="carousel-item">
+            <img src="assets/img/theGhoul_gif.gif" class="d-block w-100" alt="TheGhoul_carrusel">
+            <div class="carousel-caption d-none d-md-block">
+                <div class="card" style="width: 18rem;">
+                <img src="assets\img\theGhoul.jpg" class="card-img-top">
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item">
-      <img src="assets/img/duckSoup_gif.gif" class="d-block w-100" alt="duckSoup_carrusel">
-      <div class="carousel-caption d-none d-md-block">
-        <div class="card" style="width: 18rem;">
-          <img src="assets\img\duckSoup.jpg" class="card-img-top">
+        <div class="carousel-item">
+            <img src="assets/img/duckSoup_gif.gif" class="d-block w-100" alt="duckSoup_carrusel">
+            <div class="carousel-caption d-none d-md-block">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets\img\duckSoup.jpg" class="card-img-top">
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>

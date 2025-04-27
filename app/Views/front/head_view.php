1ada56d1b921edad074e5734f38e8fd9b744d0c7
Toia <victoriacool124@gmail.com>
2025-04-27 14:46:31 -0300

    arreglo del logo

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -5,7 +5,7 @@
         <link href="assets/css/miestilo.css" rel="stylesheet">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <script src="assets/js/bootstrap.bundle.min.js"></script>
-        <link rel="icon" href="assets/img/logo.png" type="image/x-icon">
+        <link rel="icon" href="assets\img\logo.png" type="image/x-icon">
         <title>Rebobinar</title>
     </head>
 <body>

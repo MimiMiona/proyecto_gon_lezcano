2969f76ecd19d582d96eca4b38ffe4499a539da3
Toia <victoriacool124@gmail.com>
2025-05-16 15:19:37 -0300

    formulario

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,3 +14,8 @@ $routes->get('comercializacion', 'Home::comercializacion');
 $routes->get('terminos', 'Home::terminos');
 $routes->get('privacidad', 'Home::privacidad');
 $routes->get('preguntas_frecuentes', 'Home::preguntas_frecuentes');
+
+$routes->get('/login', 'Home::login');
+$routes->post('/enviarlogin', 'Login_controller::auth');
+$routes->get('/login', 'Panel_controller::index', ['filter' => 'auth']);
+$routes->get('/logout', 'Login_controller::logout');
---
+++ app/Controllers/login_controller.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+use App\Models\Usuarios_model;
+
+class Login:controller extends BaseController
+{
+	public function index()
+	{
+		helper(['form', 'url']);
+	}
+	public function auth()
+	{
+		$session = session();
+		$model = new Usuarios_model();
+		$email = $this->request->getVar('email');
+		$password = $this->request->getVar('pass');
+		$data = $model->where('email', $email)->first();
+		if($data){
+			
+            $pass = $data['pass'];
+			$ba = $data['baja'];
+            if($ba == 'SI'){
+				
+                $session->setFlashdata('msg', 'usuario dado de baja');
+				return redirect()->to('/');
+				$verify_pass = password_verify($password, $pass);
+				if($verify_pass){
+					
+                    $ses_data = [
+					'id_usuario' => $data['id_usuario'];
+					'nombre' => $data['nombre'];
+					'apellido' => $data['apellido'];
+					'email' => $data['email'];
+					'usuario' => $data['usuario'];
+					'perfil_id' => $data['perfil_id'];
+					'logged_in' => TRUE;
+					];
+					$session->set($ses_data)
+					session()->setFlashdata('msg', 'Bienvenido!!');
+				}else{
+					$session->setFlashdata('msg', 'Password Incorrecta');
+					return redirect()->to('/login');
+				}
+		}else{
+			$session->setFlashdata('msg', 'No ingreso un email o el mismo es incorrecto');
+			return redirect()->to('/login');
+		}
+	}
+}
